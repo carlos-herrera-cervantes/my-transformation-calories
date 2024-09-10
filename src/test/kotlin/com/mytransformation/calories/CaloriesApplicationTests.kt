da78001be5e0e03fe59be9b7e4a9d513e8ea1d26
Carlos Herrera <juan.cervantes@rappi.com>
2024-09-09 18:27:37 -0600

    MT-26 Add base project

---
+++ src/test/kotlin/com/mytransformation/calories/CaloriesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mytransformation.calories
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaloriesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
